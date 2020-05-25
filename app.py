4045e56d0f2c3baa385fc48c4000c3fa50439549
unknown <agrimnautiyal1999@gmail.com>
2020-05-25 06:12:31 +0530

    Fixed page loading issue

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/process')
+def process():
+    return True
+
 if __name__ == "__main__":
     app.run(debug=True)
